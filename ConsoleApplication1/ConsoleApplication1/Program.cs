944cc427df1accc2e5ab83378a340075e92cdf95
tdnasc <tdnasc@gmail.com>
2018-03-17 20:44:28 -0300

    Foi criado uma operação de coma no projeto.

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i1 = 10;
+            int i2 = 20;
+            int i3 = i1 + i2;
+
+            Console.WriteLine(i3);
+            Console.ReadKey();
+        }
+        
+    }
+}
